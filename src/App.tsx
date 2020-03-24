ae9cb762dd7f3b0635e9747abe7ca671bc5c3a7c
askif <yasuo.cpff@gmail.com>
2020-03-24 14:48:45 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import { GlobalStyle } from './utils/GlobalStyle'
+import logo from './logo.svg'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyle />
+
+      <Wrapper>
+        <Header>
+          <Logo src={logo} className='App-logo' alt='logo' />
+          <Text>
+            Edit <CodeText>src/App.tsx</CodeText> and save to reload.
+          </Text>
+          <OfficialLink className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+            Learn React
+          </OfficialLink>
+        </Header>
+      </Wrapper>
+    </>
+  )
 }
 
-export default App;
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Logo = styled.img`
+  height: 40vmin;
+`
+
+const OfficialLink = styled.a`
+  color: #09d3ac;
+`
+
+const Text = styled.p``
+
+const CodeText = styled.code``
+
+export default App
