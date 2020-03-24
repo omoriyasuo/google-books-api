2a7133a685c50bb719b002487838fb8855ccd9dd
askif <yasuo.cpff@gmail.com>
2020-03-24 15:09:30 +0900

    feat: manage state management with React Hooks

--- src/SearchBooks.tsx
+++ src/SearchBooks.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import styled from 'styled-components'
 
 export const SearchBooks: React.FC = () => {
-  return <Wrapper>books...</Wrapper>
+  const [text, setText] = useState('')
+
+  const handleChangeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <Wrapper>
+      <Body>
+        <Title>Search Books with Google Books API!</Title>
+
+        <TextArea placeholder='テキストを入力してね！' onChange={handleChangeText} />
+
+        <TextResult>{text}</TextResult>
+      </Body>
+    </Wrapper>
+  )
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Body = styled.div``
+
+const Title = styled.h1`
+  text-align: center;
+`
+
+const TextArea = styled.textarea`
+  display: block;
+  margin: 0 auto;
+  box-sizing: border-box;
+  width: 200px;
+`
+
+const TextResult = styled.p`
+  width: 200px;
+  padding: 10px;
+  margin: 20px auto;
+  border: 1px solid blue;
+  white-space: pre-wrap;
+  box-sizing: border-box;
+`
